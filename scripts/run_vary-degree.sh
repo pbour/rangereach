4fda526911d524c1d192c7a2bf022d5080f9d23c
Panagiotis Bouros <12070531+pbour@users.noreply.github.com>
2024-02-10 00:47:01 +0100

    Add files via upload

--- scripts/run_vary-degree.sh
+++ scripts/run_vary-degree.sh
@@ -3,20 +3,20 @@ do
     echo $d
     for m in spareach-bfl spareach-int socreach 3dreach 3dreach_rev
     do
-        for d in 0 1 2 3 4
+        for de in 0 1 2 3 4
         do
-                echo "./run_${m}.exec ./inputs/${d}/${d} ./queries/${d}/queries-range-degree.0.05-${d}.txt.qry > ./outputs/${d}/run_${m}_queries-range-degree.0.05-${d}.txt"
-                ./run_${m}.exec ./inputs/${d}/${d} ./queries/${d}/queries-range-degree.0.05-${d}.txt.qry > ./outputs/${d}/run_${m}_queries-range-degree.0.05-${d}.txt
+                echo "./run_${m}.exec ./inputs/${d}/${d} ./queries/${d}/queries-range-degree.0.05-${de}.txt.qry > ./outputs/${d}/run_${m}_queries-range-degree.0.05-${de}.txt"
+                ./run_${m}.exec ./inputs/${d}/${d} ./queries/${d}/queries-range-degree.0.05-${de}.txt.qry > ./outputs/${d}/run_${m}_queries-range-degree.0.05-${de}.txt
         done
         echo
     done
 
     for m in spareach-int_MBR
     do
-        for d in 0 1 2 3 4
+        for de in 0 1 2 3 4
         do
-                echo "./run_${m}.exec ./inputs/${d}/${d} ./queries/${d}/queries-range-degree.0.05-${d}.txt.qry > ./outputs/${d}/run_${m}_queries-range-degree.0.05-${d}.txt"
-                ./run_${m}.exec ./inputs/${d}/${d} ./queries/${d}/queries-range-degree.0.05-${d}.txt.qry > ./outputs/${d}/run_${m}_queries-range-degree.0.05-${d}.txt
+                echo "./run_${m}.exec ./inputs/${d}/${d} ./queries/${d}/queries-range-degree.0.05-${de}.txt.qry > ./outputs/${d}/run_${m}_queries-range-degree.0.05-${de}.txt"
+                ./run_${m}.exec ./inputs/${d}/${d} ./queries/${d}/queries-range-degree.0.05-${de}.txt.qry > ./outputs/${d}/run_${m}_queries-range-degree.0.05-${de}.txt
         done
         echo
     done
